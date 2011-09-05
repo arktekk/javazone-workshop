04f883cb15860e23caf10c1d2d0415b90af9bfdb
Jon-Anders Teigen <mail@jteigen.com>
2011-09-05 22:55:27 +0200

    main-class

---
+++ intro/src/main/scala/Buzzword.scala
@@ -0,0 +1,10 @@
+import io.Source
+
+/*
+http://www.scala-lang.org/api/current/index.html
+*/
+object Buzzword extends App {
+  val content = Source.fromFile("buzz", "UTF-8")
+
+  println("Hva er årets buzzword på JavaZone ?")
+}
